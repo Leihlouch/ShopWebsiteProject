e4d322b696c47d94493ed3df0f6d24dc3c48c49b
Leihlouch <leihaldrin.balbin@college.cksc.edu.ph>
2026-04-28 19:17:07 +0800

    made the login.php more secure by using prepared statement to prevent SQL injections.

--- login.php
+++ login.php
@@ -5,16 +5,19 @@ include 'login_connect.php';
 if(isset($_POST['signIn'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $password = md5($password);
 
-    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-    $result = $conn->query($sql);
-    if($result->num_rows > 0){
-        session_start();
-        $row=$result->fetch_assoc();
-        $_SESSION['email'] = $row['email'];
-        header("Location: index.php");
-        exit();
+
+    $sql = $conn->prepare('SELECT id, email, password FROM users WHERE email = ?');
+    $sql->bind_param('s', $email);
+    $sql->execute();
+
+    $result = $sql->get_result();
+    $row    = $result->fetch_assoc();
+
+    if ($row && password_verify($password, $row['password'])) {
+            $_SESSION['email'] = $row['email'];
+            header("Location: index.php");
+            exit();
     }
     else{
         echo "Not Found, Incorrect Email or Password";
